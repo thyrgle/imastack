b345180522d53897ffbb593d53b952f3c55c528e
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-10-31 19:08:45 -0700

    Better documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,118 @@
+/// Extracts two values off the top of a stack. 
+///
+/// # Arguments
 macro_rules! get_ops {
-    ($env:expr) => {
-        ($env.pop().unwrap_or(0.0),
-         $env.pop().unwrap_or(0.0))
+    ($stack:expr) => {
+        ($stack.pop().unwrap_or(0.0),
+         $stack.pop().unwrap_or(0.0))
     }
 }
 
-fn parse_number(token: &str, env: &mut Vec<f64>) {
-    let result = token.parse::<f64>().unwrap_or(0.0);
-    env.push(result);
+fn parse_number(token: &str, stack: &mut Vec<f64>) {
+    let number = token.parse::<f64>().unwrap_or(0.0);
+    stack.push(number);
 }
 
-fn add(env: &mut Vec<f64>) {
-    // Get the adders.
-    let (a, b) = get_ops!(env);
-    env.push(a + b);
+/// Pops the top two elements off the stack and adds them.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn add(stack: &mut Vec<f64>) {
+    let (a, b) = get_ops!(stack);
+    stack.push(a + b);
 }
 
-fn sub(env: &mut Vec<f64>) {
-    let (a, b) = get_ops!(env);
-    env.push(a - b);
+/// Pops the top two elements off the stack and subtracts them.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn sub(stack: &mut Vec<f64>) {
+    let (a, b) = get_ops!(stack);
+    stack.push(a - b);
 }
 
-fn mul(env: &mut Vec<f64>) {
-    let (a, b) = get_ops!(env);
-    env.push(a * b);
+/// Pops the top two elements off the stack and multiplies them.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn mul(stack: &mut Vec<f64>) {
+    let (a, b) = get_ops!(stack);
+    stack.push(a * b);
 }
 
-fn div(env: &mut Vec<f64>) {
-    let (a, b) = get_ops!(env);
+/// Pops the top two elements off the stack and divides them.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn div(stack: &mut Vec<f64>) {
+    let (a, b) = get_ops!(stack);
     if b == 0.0 {
-        env.push(0.0);
+        stack.push(0.0);
     } else {
-        env.push(a / b);
+        stack.push(a / b);
     }
 }
 
-fn dup(env: &mut Vec<f64>) {
-    let to_dup = env.pop().unwrap_or(0.0);
-    env.push(to_dup);
-    env.push(to_dup);
+/// Pops the top element off the stack and pushes two copies of it on the stack.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn dup(stack: &mut Vec<f64>) {
+    let to_dup = stack.pop().unwrap_or(0.0);
+    stack.push(to_dup);
+    stack.push(to_dup);
 }
 
-fn swp(env: &mut Vec<f64>) {
-    let (first, second) = get_ops!(env);
-    env.push(first);
-    env.push(second);
+/// Pops the top two elements off the stack and swaps their values.
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn swp(stack: &mut Vec<f64>) {
+    let (first, second) = get_ops!(stack);
+    stack.push(first);
+    stack.push(second);
 }
 
-fn jnz(i: &mut usize, env: &mut Vec<f64>) {
-    let (cond, jump) = get_ops!(env);
+/// Pops the top two elements off the stack and subtracts them. TODO
+/// # Arguments
+///
+/// * `stack` - The stack to be mutated.
+fn jnz(i: &mut usize, stack: &mut Vec<f64>) {
+    let (cond, jump) = get_ops!(stack);
     if cond != 0.0 {
         *i = jump as usize;
     }
 }
 
-fn print_float(env: &mut Vec<f64>) {
-    println!("{}", env.pop().unwrap_or(0.0));
+/// Prints the top value of a particular stack.
+///
+/// # Arguments
+/// * `stack` - The stack to be mutated.
+fn print_float(stack: &mut Vec<f64>) {
+    println!("{}", stack.pop().unwrap_or(0.0));
 }
 
-fn parse_program(tokens: Vec<&str>, env: &mut Vec<f64>) {
+/// Given a list of commands, execute the commands.
+///
+/// # Arguments
+///
+/// * `tokens` - A slice of tokens to be executed.
+/// * `stack` - The stack to keep the current state of the program.
+fn execute_program(tokens: &[&str], stack: &mut Vec<f64>) {
     let mut i: usize = 0;
     loop {
         match tokens.get(i) {
             Some(tok) => {
                 match *tok {
-                    "+"     => add(env),
-                    "-"     => sub(env),
-                    "*"     => mul(env),
-                    "/"     => div(env),
-                    "dup"   => dup(env),
-                    "swp"   => swp(env),
-                    "jnz"   => jnz(&mut i, env),
-                    "print" => print_float(env),
-                    _       => parse_number(tok, env)
+                    "+"     => add(stack),
+                    "-"     => sub(stack),
+                    "*"     => mul(stack),
+                    "/"     => div(stack),
+                    "dup"   => dup(stack),
+                    "swp"   => swp(stack),
+                    "jnz"   => jnz(&mut i, stack),
+                    "print" => print_float(stack),
+                    _       => parse_number(tok, stack)
                 }
             },
             None => break
@@ -81,8 +121,11 @@ fn parse_program(tokens: Vec<&str>, env: &mut Vec<f64>) {
     }
 }
 
+/// Evaluates a string of code.
+///
+/// * `code` - The string of code to be executed.
 pub fn eval(code: String) {
     let tokens: Vec<&str> = code.split(' ').collect();
-    let mut env: Vec<f64> = Vec::new();
-    parse_program(tokens, &mut env);
+    let mut stack: Vec<f64> = Vec::new();
+    execute_program(tokens.as_slice(), &mut stack);
 }
