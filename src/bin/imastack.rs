149de4a2e2089c11ea94ae10ef07a42632ee3533
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-12-08 12:05:46 -0800

    Better repl support.

--- src/bin/imastack.rs
+++ src/bin/imastack.rs
@@ -1,20 +1,64 @@
+//! This the REPL for the imastack language. The last output is printed at the
+//! bottom of the screen.
 extern crate imastack;
+extern crate ncurses;
 
-use std::io;
-use std::io::Write;
+use std::process;
 
 /// Simple REPL for the imastack langauge.
 fn main() {
+    ncurses::initscr();
+    ncurses::raw();
+    ncurses::noecho();
+    ncurses::keypad(ncurses::stdscr(), true);
+
+    let mut code: Vec<String> = Vec::new();
+    code.push(String::from(" "));
+    let mut current_line: usize = 0;
+    let mut current_col: usize = 0;
     loop {
-        let mut code = String::new();
-        print!("> ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut code)
-            .expect("Failed to read line");
-        let env = imastack::eval(code.trim());
-        for num in env.output {
-            print!("{} ", num);
-        }
-        println!()
+        ncurses::mv(current_line as i32, current_col as i32);
+        let ch = ncurses::getch();
+        match ch {
+            ncurses::KEY_ENTER => {
+                code.push(String::from(" "));
+                current_line += 1;
+                current_col = 0;
+            },
+            // Also enter key...
+            10 => {
+                code.push(String::from(" "));
+                current_line += 1;
+                current_col = 0;
+            },
+            ncurses::KEY_UP => {
+                match current_line {
+                    0 => { },
+                    _ => current_line -= 1,
+                };
+                current_col = 0;
+            },
+            ncurses::KEY_DOWN => {
+                if current_line == code.len() {
+                } else {
+                    current_line += 1;
+                }
+                current_col = 0;
+            },
+            // Exit with Tab key.
+            9 => process::exit(0),
+            character => {
+                code[current_line].push(character as u8 as char);
+                ncurses::addch(character as u64);
+                current_col += 1;
+            }
+        };
+        let env = imastack::eval(&code[current_line].as_str());
+        let num = env.output.last().unwrap_or(&imastack::ast::Float(0.0));
+        ncurses::mv(ncurses::LINES() - 1, 0);
+        ncurses::clrtoeol();
+        ncurses::mvprintw(ncurses::LINES() - 1,
+                          ncurses::COLS() - num.to_string().len() as i32,
+                          &mut num.to_string());
     }
 }
